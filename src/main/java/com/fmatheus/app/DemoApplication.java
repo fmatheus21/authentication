228831ba36b54f457cbaeeb89600041ff549d782
Fernando Matheus <fernando.matheuss@hotmail.com>
2022-06-12 03:57:15 -0300

    first commit

---
+++ src/main/java/com/fmatheus/app/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.fmatheus.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
